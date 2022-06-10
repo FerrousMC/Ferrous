9eba1d369b54719fde61afb6447957da6ea23968
AlbinoJawa <albinojawa@gmail.com>
2022-06-09 19:16:30 -0500

    more chunk and dimension boilerplate

--- src/world/world.rs
+++ src/world/world.rs
@@ -1,4 +1,11 @@
+use std::collections::HashMap;
+use crate::world::blocks::BlockState;
+
 pub struct Dimension {
+    chunks: HashMap<(i32, i32), Chunk>, // no idea if this is okay, rip ram
+    generator: ChunkGenerator // todo, each dimension should have its own generator
+}
+impl Dimension {
     //
 }
 
@@ -18,8 +25,27 @@ impl Chunk {
 
 // 16x16x16 set of blocks. Chunks consist of several of these
 pub struct ChunkSection {
-    //
+    blocks: [[[BlockState; 16]; 16]; 16]
 }
 impl ChunkSection {
-    //
+    // Returns a reference to the BlockState at the provided coords
+    pub fn get_blockstate(&self, x: usize, y: usize, z: usize) -> &BlockState {
+        &self.blocks[x][y][z]
+    }
+
+    // Returns a mutable reference to the BlockState at the provided coords
+    pub fn get_blockstate_mut(&mut self, x: usize, y: usize, z: usize) -> &mut BlockState {
+        &mut self.blocks[x][y][z]
+    }
+
+    // Sets the given BlockState to the given coords in the chunk section
+    pub fn set_blockstate(&mut self, new_state: BlockState, x: usize, y: usize, z: usize) {
+        self.blocks[x][y][z] = new_state;
+    }
+}
+
+pub struct ChunkGenerator {
+    // options for generation
+    // ...
+    gen_queue: Vec<(i32, i32)>
 }
